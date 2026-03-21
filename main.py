352baac01c0bfb803caccc1bf8c75bbf6b09e1e2
Jozsef <fj.rower@gmail.com>
2026-03-21 02:51:10 +0100

    code

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("Hello nig")
+import pygame
+import input
+from player import Player
+from sprite import sprites, Sprite
+from map import TileKind, Map
+
+pygame.init()
+
+pygame.display.set_caption("MyCraft")
+
+screen = pygame.display.set_mode((1920, 1080))
+clear_c = (20, 151, 20)
+running = True
+player = Player("images/player.png", 0 , 0)
+tile_kinds = [
+    TileKind("dirt", "images/dirt.png", False),
+    TileKind("grass", "images/grass.png", False),
+    TileKind("wood", "images/wood.png", False)
+]
+
+map = Map("maps/start.map", tile_kinds, 32)
+#Sprite("images/tree.png",0,0)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            input.keys_down.add(event.key)
+        elif event.type == pygame.KEYUP:
+            input.keys_down.remove(event.key)
+    #update
+    player.update()
+
+
+    #draw
+    screen.fill(clear_c)
+
+    map.draw(screen)
+
+    for s in sprites:
+        s.draw(screen)
+    pygame.display.flip()
+
+    #pygame.time.delay(1)
+
+
+pygame.quit()
---
+++ sprite.py
@@ -0,0 +1,20 @@
+import pygame
+
+sprites = []
+loaded = {}
+
+class Sprite:
+    def __init__(self, image, x, y):
+        if image in loaded:
+            self.image = loaded[image]
+        else:
+            self.image = pygame.transform.scale(pygame.image.load(image), (32,32))
+            loaded[image] = self.image
+
+        self.x = x
+        self.y = y
+        sprites.append(self)
+    def delete(self):
+        sprites.remove(self)
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
